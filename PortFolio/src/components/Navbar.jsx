acaf54df9d03e3e909279f5ea8f40ea35476992f
Tejas <tejasspatell2@gmail.com>
2025-11-09 13:09:53 +0530

    modify content

--- PortFolio/src/components/Navbar.jsx
+++ PortFolio/src/components/Navbar.jsx
@@ -3,16 +3,26 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-      <nav className="main-navbar">
-        <div className="nav-logo">Aspire: Software Developer</div>
-        <ul className="nav-links">
-          <li><a href="#"><i className="fas fa-house"></i> Home</a></li>
-          <li><a href="#"><i className="fas fa-user"></i> About</a></li>
-          <li><a href="#"><i className="fas fa-code"></i> Skills</a></li>
-          <li><a href="#"><i className="fas fa-briefcase"></i> Projects</a></li>
-          <li><a href="#"><i className="fas fa-envelope"></i> Contact</a></li>
-        </ul>
-      </nav>
+    <nav className="main-navbar">
+      <div className="scroll-container">
+        <div className="scroll-text">
+          <span className="nav-logo">Aspire: Software Developer</span>
+          <span className="nav-logo">[ Code : Create : Collaborate ]</span>
+          <span className="nav-logo">Open-To-Work</span>
+          <span className="nav-logo">Aspire: Software Developer</span>
+          <span className="nav-logo">[ Code : Create : Collaborate ]</span>
+          <span className="nav-logo">Open-To-Work</span>
+        </div>
+        <div className="scroll-text">
+          <span className="nav-logo">Aspire: Software Developer</span>
+          <span className="nav-logo">[ Code : Create : Collaborate ]</span>
+          <span className="nav-logo">Open-To-Work</span>
+          <span className="nav-logo">Aspire: Software Developer</span>
+          <span className="nav-logo">[ Code : Create : Collaborate ]</span>
+          <span className="nav-logo">Open-To-Work</span>
+        </div>
+      </div>
+    </nav>
   );
 };
 
