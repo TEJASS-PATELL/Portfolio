d0aa4c155df50d4c706b46c5dcbcbe3021355d75
Tejas <tejasspatell2@gmail.com>
2026-01-19 00:09:47 +0530

    redesign portfolio

--- PortFolio/src/components/Footer.jsx
+++ PortFolio/src/components/Footer.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './Footer.css';
+import { FaGithub, FaLinkedin, FaEnvelope, FaHeart } from 'react-icons/fa';
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="footer">
+    <footer className="footer-container">
       <div className="footer-content">
-        <p>&copy; 2025 Tejas Patel. All rights reserved.</p>
+        <div className="footer-left">
+          <p className="copyright">
+            &copy; {currentYear} <span className="footer-name">Tejas Patel</span>. 
+            All rights reserved.
+          </p>
+        </div>
+
+        <div className="footer-center">
+           <p className="made-with">
+             Made with <FaHeart className="heart-icon" /> by Tejas Patel
+           </p>
+        </div>
       </div>
     </footer>
-  )
+  );
 }
